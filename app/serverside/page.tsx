51e28b206c10858f4221282d6dc69fdd35cf9bef
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2024-12-14 14:09:55 -0800

    Printing header info

--- app/serverside/page.tsx
+++ app/serverside/page.tsx
@@ -1,27 +1,29 @@
-import { headers } from 'next/headers'
-import geoip from 'geoip-lite';
+import { headers } from "next/headers";
+import geoip from "geoip-lite";
 
 export default async function Page() {
-    const headersList = await headers();
+  const headersList = await headers();
 
-    const userAgent = headersList.get('user-agent');
-    const ip = headersList.get('x-real-ip');
+  const userAgent = headersList.get("user-agent");
+  const ip = headersList.get("x-real-ip");
 
-    if (!ip) {
-        return <div>IP Address not found</div>;
-    }
-    const location = geoip.lookup(ip);
+//   if (!ip) {
+//     return <div>IP Address not found</div>;
+//   }
+//   const location = geoip.lookup(ip);
 
-    return (
-        <div>
-            <h1>Request Information</h1>
-            <p>User Agent: {userAgent}</p>
-            <p>IP: {ip}</p>
-            {location && (
-                <p>
-                    Location: {location.city}, {location.region}, {location.country}
-                </p>
-            )}
-        </div>
-    );
+  return (
+    <div>
+      <h1>Request Information</h1>
+      <p>User Agent: {userAgent}</p>
+      <p>IP: {ip}</p>
+      <div>
+        {Array.from(headersList).map(([name, value]) => (
+          <p key={name}>
+            {name}: {value}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 }
