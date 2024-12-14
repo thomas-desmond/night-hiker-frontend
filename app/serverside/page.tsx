f208a08fbc18ef193865fd7b121fed58ae1d161b
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2024-12-14 14:04:32 -0800

    Add geoip-lite to get location from IP

--- app/serverside/page.tsx
+++ app/serverside/page.tsx
@@ -1,15 +1,27 @@
 import { headers } from 'next/headers'
+import geoip from 'geoip-lite';
 
 export default async function Page() {
-    const headersList = await headers()
+    const headersList = await headers();
 
-    const userAgent = headersList.get('user-agent')
-    const ip = headersList.get('x-real-ip')
-  return (
-    <div>
-      <h1>Request Information</h1>
-      <p>User Agent: {userAgent}</p>
-      <p>ip: {ip}</p>
-    </div>
-  )
+    const userAgent = headersList.get('user-agent');
+    const ip = headersList.get('x-real-ip');
+
+    if (!ip) {
+        return <div>IP Address not found</div>;
+    }
+    const location = geoip.lookup(ip);
+
+    return (
+        <div>
+            <h1>Request Information</h1>
+            <p>User Agent: {userAgent}</p>
+            <p>IP: {ip}</p>
+            {location && (
+                <p>
+                    Location: {location.city}, {location.region}, {location.country}
+                </p>
+            )}
+        </div>
+    );
 }
