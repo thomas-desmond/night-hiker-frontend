e9b4fbed0a8f0fc29491707c7f9a1093ae3742ca
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2024-12-14 13:58:18 -0800

    First push of serverside page testing

---
+++ app/serverside/page.tsx
@@ -0,0 +1,15 @@
+import { cookies, headers } from 'next/headers'
+
+export default async function Page() {
+    const headersList = await headers()
+
+    const userAgent = headersList.get('user-agent')
+    const ip = headersList.get('x-real-ip')
+  return (
+    <div>
+      <h1>Request Information</h1>
+      <p>User Agent: {userAgent}</p>
+      <p>ip: {ip}</p>
+    </div>
+  )
+}
